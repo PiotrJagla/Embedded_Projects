456ca7df2d3b94ea31e5151911bad942174ccbf1
PiotrJagla <piotrek2002.09@gmail.com>
2023-12-28 17:30:26 +0100

    improvments for snake game

--- stm32/snake_game/snake_game/smakeGame.c
+++ stm32/snake_game/snake_game/smakeGame.c
@@ -7,9 +7,13 @@
 #include "snakeGame.h"
 #include "gpio.h"
 #include "FreeRTOS.h"
+#include "usart.h"
 
 uint8_t gameMatrix[8] = {0,32,0,0,0,0,0,0};
-struct player snake = {2,4,1,0};
+struct player snake = {
+		{2,0,0,0,0,0,0,0,0,0,0},
+		{4,0,0,0,0,0,0,0,0,0,0},
+		1,0};
 uint8_t isInGame = 0;
 
 void pushRandomDot() {
@@ -22,35 +26,47 @@ void pushRandomDot() {
 
 
 void initSnakeGame(){
+	//reset game matrix
 	for(uint8_t i = 0 ; i < MATRIX_SIZE ; ++i) {
 		gameMatrix[i] = 0;
 	}
-	pushRandomDot();
-	snake.xPos = 2;
-	snake.yPos = 4;
+	//reset snake positions
+	for(uint8_t i = 1; i < 10; ++i) {
+		snake.xPositions[i] = 0;
+		snake.yPositions[i] = 0;
+	}
+	snake.xPositions[0] = 2;
+	snake.yPositions[0] = 4;
 	snake.xDir = 1;
 	snake.yDir = 0;
-	gameMatrix[snake.yPos] |= (1<<snake.xPos);
+
+	isInGame = 1;
+	pushRandomDot();
+	gameMatrix[snake.yPositions[0]] |= (1<<snake.xPositions[0]);
+
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET); //R
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET);   //G
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET); //B
 }
 
 void updateSnakeGame(){
 	if(isInGame == 1) {
 		//Remove previous position
-		gameMatrix[snake.yPos] &= ~(1<<snake.xPos);
+		gameMatrix[snake.yPositions[0]] &= ~(1<<snake.xPositions[0]);
 
 		//Update pos
-		snake.xPos += snake.xDir;
-		snake.yPos += snake.yDir;
+		snake.xPositions[0] += snake.xDir;
+		snake.yPositions[0] += snake.yDir;
 
 		//Is out of bounds
-		if(snake.xPos >= MATRIX_SIZE) {
+		if(snake.xPositions[0] >= MATRIX_SIZE) {
 			isInGame = 0;
-		} else if(snake.xPos < 0) {
+		} else if(snake.xPositions[0] < 0) {
 			isInGame = 0;
 		}
-		if(snake.yPos >= MATRIX_SIZE) {
+		if(snake.yPositions[0] >= MATRIX_SIZE) {
 			isInGame = 0;
-		} else if(snake.yPos < 0) {
+		} else if(snake.yPositions[0] < 0) {
 			isInGame = 0;
 		}
 		if(isInGame == 0) {
@@ -60,12 +76,22 @@ void updateSnakeGame(){
 		}
 
 		//dot collected
-		if(gameMatrix[snake.yPos] & (1<<snake.xPos)) {
+		if(gameMatrix[snake.yPositions[0]] & (1<<snake.xPositions[0])) {
 			pushRandomDot();
+
+			//blink when dot is collected
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET); //R
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET);   //G
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET); //B
+			osDelay(50);
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET); //R
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET);   //G
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET); //B
 		}
 
+
 		//Draw new position
-		gameMatrix[snake.yPos] |= (1<<snake.xPos);
+		gameMatrix[snake.yPositions[0]] |= (1<<snake.xPositions[0]);
 	}
 }
 
--- stm32/snake_game/snake_game/snakeGame.h
+++ stm32/snake_game/snake_game/snakeGame.h
@@ -13,11 +13,13 @@
 #define MATRIX_SIZE 8
 
 struct player{
-	uint8_t xPos;
-	uint8_t yPos;
+	uint8_t xPositions[10];
+	uint8_t yPositions[10];
 	uint8_t xDir;
 	uint8_t yDir;
+	uint8_t points;
 };
+
 
 extern struct player snake;
 extern uint8_t gameMatrix[];
