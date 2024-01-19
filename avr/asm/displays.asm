ad4418fb92cd1081e8cd8cf32dc62d22ba4c913a
PiotrJagla <piotrek2002.09@gmail.com>
2024-01-19 10:05:50 +0100

    making some asm stuff with atmega328p

---
+++ avr/asm/displays.asm
@@ -0,0 +1,31 @@
+
+.nolist
+.include "/usr/share/avra/m328Pdef.inc"
+.list
+
+.def		temp = r16
+
+rjmp Init
+
+Init:
+				ser temp
+				out DDRB, temp
+
+				ldi temp, 0b11111110
+
+				out DDRD, temp
+
+				clr temp
+				out PortB, temp
+				ldi temp, 0b00000001
+				out PortD, temp
+
+
+				rjmp Main 
+
+Main:
+				in temp,PinD
+
+				out PortB, temp
+				 
+				rjmp Main
